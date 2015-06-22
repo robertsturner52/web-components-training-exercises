946cc45c6516549a7c7a26012d552942bff624d9
robertsturner52 <robertsturner52@gmail.com>
2015-06-22 14:59:44 -0700

    added hello badge to styleguide

--- nesting-components/accordion_component/script.js
+++ nesting-components/accordion_component/script.js
@@ -13,3 +13,19 @@ accordions.forEach(function(accordion) {
     });
   });
 });
+
+var accordions2 = Array.prototype.slice.call(document.querySelectorAll('.accordion-component2'));
+accordions2.forEach(function(accordion) {
+  var allContent = Array.prototype.slice.call(accordion.querySelectorAll('.content'));
+  var sections = Array.prototype.slice.call(accordion.querySelectorAll('.section'));
+  sections.forEach(function(section) {
+    var content = section.querySelector('.content');
+    section.querySelector('h2').addEventListener('click', function() {
+      var closed = content.classList.contains('closed');
+      allContent.forEach(function(content) {
+        content.classList.add('closed');
+      });
+      closed ? content.classList.remove('closed') : null;
+    });
+  });
+});
